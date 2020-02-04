1302253895364ccd01debb8de24fc48d4ed11170
Malav Rana <60441320+ranam13@users.noreply.github.com>
2020-02-04 16:27:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void displayToast(View v) {
+        Toast.makeText(MainActivity.this, "Welcome to the app", Toast.LENGTH_SHORT).show();
+    }
+}
